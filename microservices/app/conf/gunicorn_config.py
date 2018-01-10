956f6b6d187ebed9de6eb88ad4a71e4aa8398a38
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2018-01-10 10:22:31 +0530

    change number of workers to 1

--- microservices/app/conf/gunicorn_config.py
+++ microservices/app/conf/gunicorn_config.py
@@ -1,7 +1,10 @@
 import multiprocessing
 
 bind = "0.0.0.0:8080"
-workers = (multiprocessing.cpu_count() * 2) + 1
+# set number of workers based on CPU - good for production
+# workers = (multiprocessing.cpu_count() * 2) + 1
+# for dev, set worker = 1
+workers = 1
 accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = "debug"
