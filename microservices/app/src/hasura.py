1ac574937bee0a95c64b1f17afeaa35c848890f3
Tanmai Gopal <tanmaig@34cross.in>
2018-02-14 19:47:07 +0530

    Add upload file..

--- microservices/app/src/hasura.py
+++ microservices/app/src/hasura.py
@@ -68,3 +68,52 @@ def user_info():
                 'user_id': request.headers['X-Hasura-User-Id'],
                 'roles': request.headers['X-Hasura-Allowed-Roles']
             })
+
+@app.route("/examples/user-files")
+def files():
+    """
+        Sample endpoint that allows logged in users to upload files
+        and shows users the files they've uploaded.
+    """
+    print (str(request.headers))
+
+    # Local development will need you to add custom headers
+    if 'x-hasura-allowed-roles' not in [x.lower() for x in request.headers.keys()]:
+        return """This route can only be accessed
+            via the Hasura API gateway.
+            Deploy with <code>git push</code> and then test this route."""
+
+    # If user is not logged in (via Hasura's auth)
+    if ('anonymous' in request.headers['x-hasura-allowed-roles']):
+        return render_template('anonymous_file.html',
+            **{'base_domain': request.headers['X-Hasura-Base-Domain']})
+
+    # If user is logged in, show the user files they have uploaded
+    else:
+        # Query from the fileupload table
+        requestPayload = {
+            "type": "select",
+            "args": {
+                "table": {
+                    "name": "hf_file",
+                    "schema": "hf_catalog"
+                },
+                "columns": [ "*" ],
+                "where": {"user_id": request.headers['x-hasura-user-id']}
+            }
+        }
+
+        # Make the query and store response in resp
+        resp = requests.post(url, data=json.dumps(requestPayload))
+
+        # resp.content contains the json response.
+        if not(resp.status_code == 200):
+            print (resp.text)
+            return "Something went wrong while trying to fetch files: " + resp.text
+
+        files = resp.json()
+        return render_template('user.html',
+            **{
+                'base_domain': request.headers['X-Hasura-Base-Domain'],
+                'files': request.headers['X-Hasura-User-Id']
+            })
