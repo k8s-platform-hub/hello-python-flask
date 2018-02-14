e9783a3e095775d815d6bb18adcf2b9a7f797011
Tanmai Gopal <tanmaig@34cross.in>
2018-02-14 15:51:15 +0530

    IC

--- microservices/app/src/hasura.py
+++ microservices/app/src/hasura.py
@@ -1,4 +1,4 @@
-import requests
+import requests, json
 from flask import jsonify, request
 from src import app
 
@@ -6,27 +6,28 @@ from src import app
 # // First: connect to Hasura Data APIs directly on port 9000
 # // $ hasura ms port-forward data -n hasura --local-port=9000
 # // Second: Uncomment the line below
-# dataUrl = 'http://localhost:9000'
+dataUrl = 'http://localhost:9000/v1/query'
 
-dataUrl = 'http://data.hasura/v1/query'
+# dataUrl = 'http://data.hasura/v1/query'
 
-@app.route("/get_articles")
+@app.route("/examples/get-data")
 def get_articles():
-    if 'hasura-app.io' in request.url_root:
+    if ('hasura-app.io' in request.url_root) or ('data.hasura' not in dataUrl):
         query = {
             "type": "select",
             "args": {
                 "table": "article",
                 "columns": [
                     "*"
-                ]
+                ],
+                "limit": 10
             }
         }
         response = requests.post(dataUrl, data=json.dumps(query))
         if response.status_code == 200:
             return jsonify(response.json())
         else:
-            return 'Something went wrong: <br/>' + response.status_code + '<br/>' + response.text
+            return 'Something went wrong: <br/>' + str(response.status_code) + '<br/>' + response.text
 
     # Change the data URL during local development
     return ("Edit the dataUrl variable in <code>microservices/app/src/hasura.py</code> to test locally.")
